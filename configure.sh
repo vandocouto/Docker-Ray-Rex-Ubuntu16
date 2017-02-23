30f912618f2369172317ab2a8a33c69ffb324deb
root <root@ip-10-0-0-11.ec2.internal>
2017-02-23 21:15:30 +0000

    ok

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 
 # instalando o Docker Engine no Ubuntu16
 apt-get update
-apt-get install apt-transport-https ca-certificates curl nfs-common -y
+apt-get install apt-transport-https ca-certificates curl nfs-common python-pip -y
 
 apt-key adv \
 --keyserver hkp://ha.pool.sks-keyservers.net:80 \
@@ -13,6 +13,10 @@ echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" | tee /etc/apt/
 apt-get update
 apt-cache policy docker-engine
 apt-get install docker-engine -y
+
+# mkdir /docker-compose
+mkdir /docker-compose
+pip install docker-compose
 
 # instalando o Rex-Ray no Ubuntu16
 curl -sSL https://dl.bintray.com/emccode/rexray/install | sh -s -- stable
